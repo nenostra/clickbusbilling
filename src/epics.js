fc2d0bb0b822136debc3725991a3ab7e99b84668
Cesar Gutierrez <cesar.gutierrez@clickbus.com>
2017-12-14 17:23:34 -0600

    added enpoint as an env variable

--- src/epics.js
+++ src/epics.js
@@ -9,20 +9,19 @@ import 'rxjs/add/observable/of';
 import { ajax } from 'rxjs/observable/dom/ajax';
 import { Observable } from 'rxjs/Observable';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiY2xpY2tidXMifQ==.YjVjNGRjM2QyYTIwMDkwY2FjNTY1YzJmOTQ4Nz'
-  + 'RhNTUyNjIwNWM2NWVjOWNlMDYwNWI2ZDAxNjQ0MTcwYTg3Yg==';
-const endpoint = 'http://api.local.clickbus.com.mx/api/v1/billing-data/';
+const token = process.env.REACT_APP_API_TOKEN;
+const endpoint = process.env.REACT_APP_API_ENDPOINT;
 
 const fetchRfcEpic = action$ =>
   action$.ofType('FETCH_RFC')
-    .mergeMap(({ payload }) => ajax.getJSON(`${endpoint}${payload}`, { token }) //TODO: CAMBIAR RUTA DEPENDIENDO AMBIENTE
+    .mergeMap(({ payload }) => ajax.getJSON(`${endpoint}${payload}`, { token })
       .map( values => ({ type: 'RFC_SUCCESS', payload: values }))
       .concat(Observable.of({ type: 'BILLING_DATA'}))
       .catch(error => {
         const url = error.request.url.split('/');
         return Observable.of({
           type: 'RFC_FAILED',
-          payload: {rfc: url[url.length - 1]} ///TODO: FIX THIS WITH A REAL RESPONSE!!!
+          payload: {rfc: url[url.length - 1]}
         })
       })
       .concat(Observable.of({ type: 'BILLING_DATA'}))
@@ -35,7 +34,7 @@ const postRfcEpic = action$ =>
       .concat(Observable.of({ type: 'SEND_BILLING'}))
       .catch(error => {
         const url = "";
-        console.log(error)
+        console.log(error);
         return Observable.of({
           type: 'RFC_FAILED',
           payload: {id: url} ///TODO: FIX THIS WITH A REAL RESPONSE!!!
